13e0dcafbffe19a4a1649de225381c1fe071961a
Samuel HOARAU <s.hoarau@student.isartdigital.com>
2022-06-04 16:30:22 +0200

    Fix interaction between laser and other object

--- Assets/Scripts/Player/LaserBehaviour.cs
+++ Assets/Scripts/Player/LaserBehaviour.cs
@@ -15,6 +15,8 @@ public class LaserBehaviour : MonoBehaviour, IInteractable
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        Debug.Log(collision.name);
+
         if(collision.CompareTag(PLAYER_TAG) || collision.CompareTag(INTERACTABLE_TAG))
             collision.GetComponent<IInteractable>().Interaction(this);
     }

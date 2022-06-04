4e4a69fc5f43dd9657ce99474316fa0cd219a4de
Thomas <grandin.thoma@gmail.com>
2022-06-04 21:29:13 +0200

    Add Void

--- Assets/Scripts/Player/Character.cs
+++ Assets/Scripts/Player/Character.cs
@@ -7,7 +7,12 @@ public class Character : MonoBehaviour, IInteractable
 {
     [SerializeField] private Transform otherPlayer = default;
     [SerializeField] private LaserBehaviour laser = default;
-    [SerializeField] private int health = 3;
+    [SerializeField] private PlayerInput playerInput = default;
+    public int health = 3;
+    public Transform startPosition;
+    public Vector3 death = new Vector3(0,0,0);
+    public Vector3 normalState = new Vector3(1, 1, 1);
+    public Vector3 decrement = new Vector3(0.7f, 0.7f, 0.7f);
     [SerializeField] private float timeChargeLaser = 2f;
     
 
@@ -79,5 +84,36 @@ public class Character : MonoBehaviour, IInteractable
     public void StopInteraction(LaserBehaviour laser)
     {
         throw new System.NotImplementedException();
+    }
+
+    private Coroutine fall;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Void")
+        {
+            //collision.gameObject.GetComponent<Character>().health = collision.gameObject.GetComponent<Character>().health - 1;
+            health = health - 1;
+            Debug.Log("HIT!!!");
+
+            playerInput.DeactivateInput();
+
+            if (fall != null) StopCoroutine(fall);
+            fall = StartCoroutine(OnFall());
+                
+        }
+    }
+
+    IEnumerator OnFall()
+    {
+        while (gameObject.transform.localScale.x >= 0)
+        {
+            
+            gameObject.transform.localScale -= decrement * Time.deltaTime;
+            yield return null;
+        }
+        gameObject.transform.position = new Vector3(0,0,0);
+        gameObject.transform.localScale = normalState;
+        playerInput.ActivateInput();
     }
 }
